8b1cd5237a60d97bbfaf974cc33c107809419b76
Anonimo501 <67207446+Anonimo501@users.noreply.github.com>
2023-02-09 10:59:49 -0500

    Update os_detector.py

--- os_detector.py
+++ os_detector.py
@@ -6,13 +6,13 @@ def main(ip):
     try:
         result = subprocess.check_output(["ping", "-c", "1", "-t", "1", ip])
         ttl = int(re.search(r"ttl=(\d+)", result.decode("utf-8")).group(1))
-        if 0 <= ttl <= 64:
+        if ttl >= 0 and ttl <= 64:
             print("Sistema operativo: Linux")
-        elif 65 <= ttl <= 128:
+        elif ttl >= 65 and ttl <= 128:
             print("Sistema operativo: Windows")
-        elif 245 <= ttl <= 247:
+        elif ttl >= 245 and ttl <= 247:
             print("Sistema operativo: FreeBSD")
-        elif 253 <= ttl <= 255:
+        elif ttl >= 253 and ttl <= 255:
             print("Sistema operativo: Solaris")
         else:
             print("Sistema operativo no identificado")
